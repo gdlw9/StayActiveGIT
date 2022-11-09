0eff23ae03d196e05bf57654bc77e2e40097442e
Marcin <marcin.godlewski2000@wp.pl>
2022-11-09 22:34:30 +0100

    app

---
+++ app/src/main/java/pl/gda/wsb/stayactive/MainActivity.java
@@ -0,0 +1,14 @@
+package pl.gda.wsb.stayactive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
